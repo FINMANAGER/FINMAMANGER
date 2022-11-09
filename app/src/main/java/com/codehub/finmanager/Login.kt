578b0b18ab621fba68411e5804c45c1fa213cfdc
kimsiele <sielekim.emmanuel@gmail.com>
2022-11-09 23:06:12 +0300

    Signup and Login Design

---
+++ app/src/main/java/com/codehub/finmanager/Login.kt
@@ -0,0 +1,29 @@
+package com.codehub.finmanager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.codehub.finmanager.databinding.FragmentLoginBinding
+
+class Login : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            tvSignUp.setOnClickListener {
+                findNavController().navigate(R.id.signUp)
+            }
+        }
+    }
+}
