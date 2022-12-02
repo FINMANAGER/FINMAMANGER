af463de03f247f574e50d7c6aa286cbe811701cc
PercyKing <57621833+Enobong-kingsley@users.noreply.github.com>
2022-12-02 13:47:15 +0100

    Transaction Details

--- app/src/main/java/com/codehub/finmanager/TransactionDetails.kt
+++ app/src/main/java/com/codehub/finmanager/TransactionDetails.kt
@@ -2,10 +2,18 @@ package com.codehub.finmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class TransactionDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_details)
+
+        //---back button---
+        val backButton: ImageButton = findViewById(R.id.backBtn)
+        backButton.setOnClickListener {
+            finish()
+        }
+        //--------
     }
 }
